d16e787f1bf503f142d5234f531a089a31538b94
Roger Chapman <rogchap@gmail.com>
2011-08-22 08:39:49 +0100

    Basic functionality done

--- NodeApi/app.js
+++ NodeApi/app.js
@@ -1,38 +1,63 @@
-
-/**
- * Module dependencies.
- */
+// Module dependencies
 
 var express = require('express');
+var fs = require('fs');
 
-var app = module.exports = express.createServer();
+// Local variables
 
-// Configuration
+var app;
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
+// Boot functions
+
+function bootModels(app, next) {
+  fs.readdir(__dirname + '/models', function(err, files){
+    if(err) throw err;
+    files.forEach(function(file){
+      var name = file.replace('.js', '');
+      var model = __dirname + '/models' + name;
+      require(model)(app);
+      next();
+    });
+  });
+}
+
+function bootControllers(app, next) {
+  fs.readdir(app.path + '/controllers', function(err, files){
+    if(err) throw err;
+    files.forEach(function(file){
+      var name = file.replace('js', '');
+      var controller = __dirname + '/controllers' + name;
+    });
+  });
+}
+
+function bootApplication(app, next) {
+  app.set('views', app.path + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-// Routes
-
-app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
+  app.use(express.static(app.path + '/public'));
+  
+  bootModels(app, function() {
+    bootControllers(app, next);
   });
-});
+}
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+// Initialize bootstrapping
+
+exports.boot = function(next){
+  app = express.createServer();
+  app.path = __dirname;
+
+  bootApplication(app, function() {
+    next(app);
+  });
+};
+
+// Start application
+
+exports.boot(function(app){
+  app.listen(3000);
+  console.log("Express server listening on port %d in %s mode", app.address().port, global.process.env.NODE_ENV || 'development');
+});
--- NodeDrawing/app.js
+++ NodeDrawing/app.js
@@ -3,7 +3,8 @@
  * Module dependencies.
  */
 
-var express = require('express');
+var express = require('express'),
+	socketio = require('socket.io');
 
 var app = module.exports = express.createServer();
 
@@ -11,11 +12,19 @@ var app = module.exports = express.createServer();
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view options', {layout:false});
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+  
+  app.register('.html', {
+  	compile:function(str, options){
+  		return function(local){
+  			return str;
+  		};
+  	}
+  });
 });
 
 app.configure('development', function(){
@@ -29,10 +38,62 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
-  });
+	
+	var ua = req.header('user-agent');
+	if(/mobile/i.test(ua)) {
+		res.render('mobile.html');
+	} else {
+		res.render('desktop.html');
+	}
 });
 
 app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+console.log("Server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+// private methods
+
+randomCodeNumber = function() {
+	return Math.round(Math.random() * 9000) + 1000;
+};
+
+// Sockets 
+
+var io = socketio.listen(app);
+
+var users = {};
+
+var desktop = io.of('/desktop');
+var mobile = io.of('/mobile');
+
+desktop.on('connection', function(client){
+	
+	var code = randomCodeNumber();
+	users[code] = {'desktop':client, 'mobile':null};
+	client.emit('code', code);
+});
+
+mobile.on('connection', function(client){
+	
+	client.on('set code', function(code, fn){
+
+		if (users[code]) {
+			console.log(code);
+			client.set('code', code, function(){
+				users[code].mobile = client;
+				users[code].desktop.emit('mobile connected');
+				if(fn) fn();
+			});
+			
+		} else {
+			
+		};
+	});
+	
+	client.on('touchEvent', function(data){
+		client.get('code', function(err, code){
+			users[code].desktop.emit('touchEvent', data);
+		});
+	});
+	
+});
+
